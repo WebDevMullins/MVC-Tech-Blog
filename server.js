571f26c8bc3d1bcf475fd1c27a683bde237ec76a
Brandon Mullins <webdevmullins@gmail.com>
2023-11-09 09:29:00 -0800

    set up handlebars with home page

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const sequelize = require('./config/connection')
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+const hbs = exphbs.create()
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => res.render('home'))
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log(`App now listening on port ${PORT}`))
