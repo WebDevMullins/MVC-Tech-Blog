8d1663e4c0d9c1000c43a7ba3d563b56992e8c3a
Brandon Mullins <webdevmullins@gmail.com>
2023-11-09 13:05:56 -0800

    adding controllers and get all posts on homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+const { Post, User } = require('../models')
+
+router.get('/', async (req, res) => {
+	try {
+		// Get all posts and JOIN with user data
+		const postData = await Post.findAll({
+			include: [
+				{
+					model: User,
+					attributes: ['name']
+				}
+			]
+		})
+		// Serialize data
+		const posts = postData.map((post) => post.get({ plain: true }))
+
+		// Pass data into template
+		res.render('home', {
+			posts
+		})
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const sequelize = require('./config/connection')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const path = require('path')
+const sequelize = require('./config/connection')
+const routes = require('./controllers')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -15,7 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.render('home'))
+app.use(routes)
+// app.get('/', (req, res) => res.render('home'))
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log(`App now listening on port ${PORT}`))
