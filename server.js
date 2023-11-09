98ce5a6077cc72e9fbd6963c272330a7f167eb6a
Brandon Mullins <webdevmullins@gmail.com>
2023-11-09 13:40:48 -0800

    update home layout

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const exphbs = require('express-handlebars')
 const path = require('path')
 const sequelize = require('./config/connection')
 const routes = require('./controllers')
+const helpers = require('./utils/helpers')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
-const hbs = exphbs.create()
+const hbs = exphbs.create({ helpers })
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
