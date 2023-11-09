2e86252973d828b4a6b367d4305ed8926aa85e79
Brandon Mullins <webdevmullins@gmail.com>
2023-11-09 11:34:36 -0800

    add tailwindcss

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const sequelize = require('./config/connection')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -9,6 +10,10 @@ const hbs = exphbs.create()
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('home'))
 
