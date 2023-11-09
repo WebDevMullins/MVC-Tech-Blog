563d8410f0e82e089e41e820d4e46480571c1ae4
Brandon Mullins <webdevmullins@gmail.com>
2023-11-09 08:06:34 -0800

    add all models

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const Comment = require('./Comment')
+const Post = require('./Post')
+const User = require('./User')
+
+Comment.belongsTo(Post, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade'
+})
+
+Comment.belongsTo(User, {
+	foreignKey: 'post_id',
+	onDelete: 'cascade'
+})
+
+Post.belongsTo(User, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade'
+})
+
+Post.hasMany(Comment, {
+	foreignKey: 'post_id',
+	onDelete: 'cascade'
+})
+
+User.hasMany(Comment, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade'
+})
+
+User.hasMany(Post, {
+	foreignKey: 'user_id'
+})
+
+module.exports = { Comment, Post, User }
