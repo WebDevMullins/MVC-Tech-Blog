7369d51b6f62931897e5416ead098ccfbc79a4f2
Brandon Mullins <webdevmullins@gmail.com>
2023-11-13 16:17:37 -0800

    add comment section and routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -27,4 +27,38 @@ router.get('/', async (req, res) => {
 	}
 })
 
+router.get('/:id', async (req, res) => {
+	try {
+		const postData = await Post.findOne({
+			where: {
+				id: req.params.id
+			},
+			attributes: ['id', 'content', 'date_created', 'post_id', 'user_id'],
+			include: [
+				{
+					model: User,
+					attributes: ['name']
+				},
+				{
+					model: Comment,
+					attributes: ['id', 'content', 'date_created', 'post_id', 'user_id'],
+					include: {
+						model: User,
+						attributes: ['name']
+					}
+				}
+			]
+		})
+		if (!postData) {
+			res.status(404).json({ message: 'No post found with this id' })
+			return
+		}
+		const post = postData.get({ plain: true })
+		console.log(post)
+		res.render('post', { post, logged_in: req.session.logged_in })
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
 module.exports = router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Post, User } = require('../models')
+const { Comment, Post, User } = require('../models')
 
 router.get('/', async (req, res) => {
 	try {
@@ -39,6 +39,39 @@ router.get('/signup', (req, res) => {
 		return
 	}
 	res.render('signup')
+})
+
+router.get('/post/:id', async (req, res) => {
+	try {
+		const postData = await Post.findOne({
+			where: {
+				id: req.params.id
+			},
+			attributes: ['id', 'title', 'content', 'date_created', 'user_id'],
+			include: [
+				{
+					model: Comment,
+					attributes: ['id', 'content', 'date_created', 'post_id', 'user_id'],
+					include: {
+						model: User,
+						attributes: ['name']
+					}
+				},
+				{
+					model: User,
+					attributes: ['name']
+				}
+			]
+		})
+		if (!postData) {
+			res.status(404).json({ message: 'No post found with this id' })
+			return
+		}
+		const post = postData.get({ plain: true })
+		res.render('post', { post, logged_in: req.session.logged_in })
+	} catch (err) {
+		res.status(500).json(err)
+	}
 })
 
 module.exports = router
