c7720918371170c7995678475c48c534b63d17d3
WebDevMullins <webdevmullins@gmail.com>
2023-11-15 15:11:48 -0600

    add edit post route, js, and hbr

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -75,4 +75,27 @@ router.get('/:id', withAuth, async (req, res) => {
 	}
 })
 
+router.put('/:id', withAuth, async (req, res) => {
+	try {
+		const postData = await Post.update(
+			{
+				title: req.body.title,
+				content: req.body.content
+			},
+			{
+				where: {
+					id: req.params.id
+				}
+			}
+		)
+		if (!postData) {
+			res.status(404).json({ message: 'No post found with this id' })
+			return
+		}
+		res.status(200).json(postData)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
 module.exports = router
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -36,4 +36,39 @@ router.get('/new', withAuth, (req, res) => {
 	res.render('new-post')
 })
 
+router.get('/edit/:id', withAuth, async (req, res) => {
+	try {
+		const postData = await Post.findOne({
+			where: {
+				id: req.params.id
+			},
+			attributes: ['id', 'title', 'content', 'date_created'],
+			include: [
+				{
+					model: User,
+					attributes: ['name']
+				},
+				{
+					model: Comment,
+					attributes: ['id', 'content', 'date_created', 'post_id', 'user_id'],
+					include: {
+						model: User,
+						attributes: ['name']
+					}
+				}
+			]
+		})
+		if (!postData) {
+			res.status(400).json({ message: 'No post found with this id' })
+			return
+		}
+		const post = postData.get({ plain: true })
+		console.log(post)
+		res.render('edit-post', { post, logged_in: req.session.logged_in })
+	} catch (err) {
+		console.log(err)
+		res.status(500).json(err)
+	}
+})
+
 module.exports = router
